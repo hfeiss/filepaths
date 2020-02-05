abab847e15c8f182f8c53ea23d413ccf9c5b3446
hfeiss <hfeiss@gmail.com>
2020-02-05 09:29:01 -0700

    Fix file name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='filepaths',
-    version="0.0.3",
+    version="0.0.4",
     author='Hunter Feiss',
     author_email='hfeiss@gmail.com',
     description='Root-agnostic filepath generation',
